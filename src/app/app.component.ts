d6af10cb3d668b66bc3635432019495ce040df7f
lucasdvillegas <lucasdvillegas@hotmail.com>
2023-02-01 20:10:37 -0300

    subiendo la app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UsuarioService } from './services/usuario.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'cardUser';
+  user:any;
+  num:number = Math.floor(Math.random());
+
+
+  constructor( private usuarioService: UsuarioService){
+
+  }
+
+  ngOnInit(): void {
+    this.recuperarPersona();
+  }
+
+  recuperarPersona(){
+    this.usuarioService.obtenerUsuario()
+      .subscribe(resultado => this.user = resultado);
+  }
+
+  actualizar() {
+    this.recuperarPersona();
+  }
+
+  // randomUser(){
+  //   return this.num = Math.floor(Math.random() * (20 - 1)) + 1;
+  // }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    MatCardModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
